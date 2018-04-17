6da1631c5a7a4d80802a63d04269fd249dee62a2
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-04-17 18:53:59 +0530

    Commit

---
+++ Java codes/ListDuplicateExample.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class ListDuplicateExample {
+	public static void main(String args[]) {
+
+		List<String> list = createList();
+		System.out.println("List : " + list);
+		Collections.sort(list);
+		System.out.println("Sorted List : " + list);
+		findDuplicateUsingCounter(list);
+		findDuplicatesUsingSet(list);
+	}
+
+	public static void findDuplicateUsingCounter(List<String> list) {
+		List<String> duplicateList = new ArrayList<String>();
+		int counter = 0;
+		for (int i = counter; i < list.size(); i++) {
+			if (counter >= 1 && (list.get(counter - 1).equalsIgnoreCase((list.get(counter))))) {
+				duplicateList.add(list.get(counter));
+			}
+			counter++;
+		}
+		System.out.println("Duplicate using findDuplicateUsingCounter: " + duplicateList);
+	}
+
+	public static void findDuplicatesUsingSet(List<String> list) {
+		final Set<String> setToReturn = new HashSet<String>();
+		final Set<String> set1 = new HashSet<String>();
+		for (int i=0; i < list.size(); i++) {
+			if (!set1.add(list.get(i))) {
+				setToReturn.add(list.get(i));
+			}
+		}
+		System.out.println("Duplicate using findDuplicatesUsingSet: " + setToReturn);
+	}
+
+	public static List<String> createList() {
+		List<String> list = new LinkedList<String>();
+		int noOfItems = 10;
+		for (int i = 0; i < noOfItems; i++)
+			list.add("item" + i);
+
+		list.add("item" + 2);
+		list.add("item" + 4);
+		list.add("item" + 1);
+		return list;
+	}
+}
--- Java codes/ListMissingElement.java
+++ Java codes/ListMissingElement.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class ArrayListMissingElement{
+class ListMissingElement{
 	public static void main(String arg[])	{
 		Integer[] array1 = { 3, 7, 2, 90, 34, 67 };
 		Integer[] array2 = { 2, 7, 34, 3, 67};
@@ -14,14 +14,11 @@ class ArrayListMissingElement{
 		for (int i = 0; i < array2.length; i++) {
 			xor ^= array2[i];
 		}
-		System.out.println("missing number: " + xor);
+		System.out.println("Missing Number using xor : " + xor);
 
 		//Using collections
 		List list1 = Arrays.asList(array1);
 		List list2 = Arrays.asList(array2);
-
-		System.out.println("List1 : " + list1);
-		System.out.println("List2 : " + list2);
 
 		Arrays.sort(array1);
 		List list3 = Arrays.asList(array1);
@@ -29,6 +26,6 @@ class ArrayListMissingElement{
 
 		Set<Integer> set = new HashSet<Integer>(list1);
 		set.removeAll(list2);
-		System.out.println("Missing Number : " + set);
+		System.out.println("Missing Number using set : " + set);
  	}
 }
