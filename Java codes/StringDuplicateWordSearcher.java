2505fa103b8fee6f68f1887c45fc5d97e6571191
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-05-15 16:41:50 +0530

    Commit

---
+++ Java codes/StringDuplicateWordSearcher.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class StringDuplicateWordSearcher {
+	public static void main(String[] args) {
+
+		String text = "a,abc,xyz,a,abc,z,a";
+		List<String> list = Arrays.asList(text.split(","));
+
+		Set<String> uniqueWords = new HashSet<String>(list);
+		for (String word : uniqueWords) {
+			System.out.println(word + ": " + Collections.frequency(list, word));
+		}
+	}
+}
