6457f80869cb81fa4dbe433b3148aef0869f6b06
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-05-18 14:56:07 +0530

    Commit

--- Java codes/ArrayNumberCloseToZero.java
+++ Java codes/ArrayNumberCloseToZero.java
@@ -4,15 +4,16 @@ public class ArrayNumberCloseToZero {
 
 	public static void main(String[] a) {
 
-		int[] array = { 5, -2, -1, 1, 12 };
+		int[] array = {0, 5, -2, 1, 12};
 		Arrays.sort(array);
 		int num = array[0];
 		int absNum = Math.abs(num);
-	System.out.println("absNum : " + absNum);
-		for (int i = 1; i < array.length; ++i) {
+		for (int i = 1; i < array.length; i++) {
 			int newAbs = Math.abs(array[i]);
-			System.out.println("newAbs : " + newAbs);
-			if (newAbs < absNum) {
+			if(newAbs == 0){
+				num = array[i];
+				break;
+			}else if (newAbs < absNum) {
 				absNum = newAbs;
 				num = array[i];
 			}
