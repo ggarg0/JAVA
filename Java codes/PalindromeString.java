abbb95e4569a78cacdb2a0c0614649695e44d337
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-06-04 15:33:57 +0530

    Commit

---
+++ Java codes/PalindromeString.java
@@ -0,0 +1,20 @@
+public class PalindromeString {
+	public static void main(String[] a) {
+		String str = "123321";
+		char[] arr = str.toCharArray();
+		boolean isPalindrome = true;
+		int left = 0;
+		int right = (arr.length-1);
+
+		for (int i = 0; left<right; left++, right--) {
+			if (arr[left] == arr[right]) {
+				continue;
+			}
+			else {
+				isPalindrome = false;
+				break;
+			}
+		}
+		System.out.println("Is Palindrome : " + isPalindrome);
+	}
+}
