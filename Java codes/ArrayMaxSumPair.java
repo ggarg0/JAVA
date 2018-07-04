320a2384d296c86149e7e8ca252e8a2c17ce1f06
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-07-04 18:21:56 +0530

    Commit

---
+++ Java codes/ArrayMaxSumPair.java
@@ -0,0 +1,25 @@
+class ArrayMaxSumPair {
+	static int arr[] =  { 12, 34, 10, 6, 40 };
+	static int first, second;
+	static void findLargestSumPair() {		
+		if (arr[0] > arr[1]) {
+			first = arr[0];
+			second = arr[1];
+		} else {
+			first = arr[1];
+			second = arr[0];
+		}
+		for (int i = 2; i < arr.length; i++) {
+			if (arr[i] > first) {
+				second = first;
+				first = arr[i];
+			}
+			else if (arr[i] > second && arr[i] != first)
+				second = arr[i];
+		}
+	}
+	public static void main(String[] args) {
+		findLargestSumPair();
+		System.out.println("Max Pair Sum is " + (first + second));
+	}
+}
