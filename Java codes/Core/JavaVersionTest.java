e687512ab7ce80a5211fa8b96c950bbc7e5f428b
Gaurav Garg <gaurav.garg@kronos.com>
2019-01-09 17:39:17 +0530

    Commit

--- Java codes/Core/JavaVersionTest.java
+++ Java codes/Core/JavaVersionTest.java
@@ -3,7 +3,7 @@ public class JavaVersionTest
   public static void main(String[] args)
   {
     String javaVersion = System.getProperty("java.version");
-
+	System.out.format("Java Version = '%s'", javaVersion);
     if (javaVersion.startsWith("1.7"))
 	{
 	  System.out.format("Java Version = '%s'", javaVersion);
---
+++ Java codes/Core/Test.java
@@ -0,0 +1,13 @@
+public class Test
+{
+public static void main(String arg[])
+{
+
+	boolean a = true;
+	boolean b = false;
+
+	if (a == b){
+		System.out.println("Yes");
+	}
+}
+}
