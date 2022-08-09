611369dfefac4652725ee8dfa398d541cf3eefa6
Gaurav Garg <gaurav.garg@ukg.com>
2022-08-09 15:50:57 +0530

    Add

---
+++ Java codes/Implementations/BcryptExample.java
@@ -0,0 +1,11 @@
+
+public class BcryptExample {
+	public static void main(String[] args) {
+		String originalPassword = "allsomebody";
+		String generatedSecuredPasswordHash = BCrypt.hashpw(originalPassword, BCrypt.gensalt(10));
+		System.out.println(generatedSecuredPasswordHash);
+
+		boolean matched = BCrypt.checkpw(originalPassword, generatedSecuredPasswordHash);
+		System.out.println(matched);
+	}
+}
