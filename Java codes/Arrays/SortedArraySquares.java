e76d3ff8d9201d9942bbdf41f1951b1ebb339365
Gaurav Garg <gaurav.garg@ukg.com>
2022-08-23 21:00:05 +0530

    Add

---
+++ Java codes/Arrays/SortedArraySquares.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SortedArraySquares {
+
+	public static int[] makeSquares(int[] arr) {
+		int[] squares = new int[arr.length];
+		int left = 0;
+		int right = arr.length - 1;
+		int highestSquareIdx = arr.length - 1;
+
+		while (left <= right) {
+			int leftSquare = arr[left] * arr[left];
+			int rightSquare = arr[right] * arr[right];
+			if (leftSquare > rightSquare) {
+				squares[highestSquareIdx--] = leftSquare;
+				left++;
+			} else {
+				squares[highestSquareIdx--] = rightSquare;
+				right--;
+			}
+		}
+		return squares;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { -3, -1, 0, 1, 2 };
+		System.out.println(Arrays.toString(makeSquares(arr)));
+
+	}
+
+}
