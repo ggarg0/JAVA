6be40e57aa081df882073e1f5316ba5e25b2a134
Gaurav Garg <gaurav.garg@ukg.com>
2022-08-31 12:44:27 +0530

    Commit

--- Java codes/Arrays/FindFirstUniqueArray.java
+++ Java codes/Arrays/FindFirstUniqueArray.java
@@ -1,14 +1,17 @@
 package Arrays;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class FindFirstUnique {
+public class FindFirstUniqueArray {
 	public static void main(String args[]) {
 
-		int[] arr = { 2, 54, 7, 2, 6, 54};
+		// Using MAP
+		int[] arr = { 2, 54, 7, 2, 6, 54 };
 		Map<Integer, Integer> nums = new HashMap<Integer, Integer>();
-	
+
 		for (int i = 0; i < arr.length; i++) {
 			if (nums.containsKey(arr[i])) {
 				nums.put(arr[i], nums.get(arr[i]) + 1);
@@ -16,12 +19,25 @@ public class FindFirstUnique {
 				nums.put(arr[i], 1);
 			}
 		}
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			if (nums.get(arr[i]) == 1) {
 				System.out.println(arr[i]);
 				break;
 			}
 		}
+
+		// Using list
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if (list.contains(arr[i])) {
+				list.remove(new Integer(arr[i]));
+			} else {
+				list.add(arr[i]);
+			}
+		}
+
+		System.out.println(list.get(0));
+
 	}
 }
--- Java codes/Arrays/MergeOverlappingIntervals.java
+++ Java codes/Arrays/MergeOverlappingIntervals.java
@@ -7,7 +7,9 @@ import java.util.List;
 public class MergeOverlappingIntervals {
 	public static void main(String args[]) {
 		Interval[] arr = { new Interval(3, 7), new Interval(6, 8), new Interval(10, 12), new Interval(11, 15) };
-
+	//	Interval[] arr1 = { new Interval(1, 3), new Interval(2, 6), new Interval(8, 10), new Interval(15, 18) };
+		System.out.println(Arrays.toString(arr));
+		
 		List<Interval> input = Arrays.asList(arr);
 		List<Interval> output = new ArrayList<Interval>();
 		int outputCounter = 0;
