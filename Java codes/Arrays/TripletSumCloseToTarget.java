9d0b9f3c3d6cce10c6eea3ef54d980f58ea96a4f
Gaurav Garg <gaurav.garg@ukg.com>
2022-09-09 13:17:08 +0530

    Commit

---
+++ Java codes/Arrays/TripletSumCloseToTarget.java
@@ -0,0 +1,49 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TripletSumCloseToTarget {
+
+	public static void searchTriplet(int[] arr, int targetSum) {
+		Arrays.sort(arr);
+		int smallestSum = Integer.MAX_VALUE;
+		List<List<Integer>> triplets = new ArrayList<>();
+		for (int i = 0; i < arr.length - 1; i++) {
+			int left = i + 1;
+			int right = arr.length - 1;
+
+			while (left < right) {
+				int tripletSum = arr[i] + arr[left] + arr[right];
+				//System.out.println("Checking " + arr[i] + ", " + arr[left] + ", " + arr[right]);
+				if (tripletSum < smallestSum || (tripletSum > smallestSum && tripletSum < targetSum)) {
+					smallestSum = tripletSum;
+					if (triplets.isEmpty()) {
+						triplets.add(Arrays.asList(arr[i], arr[left], arr[right]));
+					} else {
+						triplets.remove(0);
+						triplets.add(Arrays.asList(arr[i], arr[left], arr[right]));
+					}
+					//System.out.println("Triplet : " + triplets + " with sum as " + smallestSum);
+				}
+
+				if (targetSum == smallestSum)
+					break;
+
+				if (smallestSum > targetSum)
+					right--;
+				else
+					left++;
+
+			}
+		}
+		System.out.println("Triplet : " + triplets + " with sum as " + smallestSum);
+	}
+
+	public static void main(String[] args) {
+		searchTriplet(new int[] { -2, 0, 1, 2 }, 2);
+		searchTriplet(new int[] { -3, -1, 1, 2 }, 1);
+		searchTriplet(new int[] { 1, 0, 1, 1 }, 1);
+	}
+}
--- Java codes/Arrays/TripletSumToZeroArray.java
+++ Java codes/Arrays/TripletSumToZeroArray.java
@@ -8,41 +8,34 @@ public class TripletSumToZeroArray {
 
 	public static List<List<Integer>> searchTriplets(int[] arr) {
 		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println("Sorted List " + Arrays.toString(arr));
 		List<List<Integer>> triplets = new ArrayList<>();
-		for (int i = 0; i < arr.length - 2; i++) {
-			if (i > 0 && arr[i] == arr[i - 1])
-				continue;
-			searchPair(arr, -arr[i], i + 1, triplets);
-		}
 
+		for (int i = 0; i < arr.length - 1; i++) 
+			searchPair(arr, -arr[i], i + 1, triplets);
+		
 		return triplets;
 	}
 
-	private static void searchPair(int[] arr, int targetSum, int left, List<List<Integer>> triplets) {
-		int right = arr.length - 1;
-		//[-5, -2, -1, 2, 3]
-		while (left < right) {
-			int currentSum = arr[left] + arr[right];
-			if (currentSum == targetSum) { // found the triplet
-				triplets.add(Arrays.asList(-targetSum, arr[left], arr[right]));
-				left++;
-				right--;
-				while (left < right && arr[left] == arr[left - 1])
-					left++;
-				while (left < right && arr[right] == arr[right + 1])
-					right--;
-			} else if (targetSum > currentSum)
-				left++;
-			else
-				right--;
+	private static void searchPair(int[] arr, int targetSum, int start, List<List<Integer>> triplets) {
+		int end = arr.length - 1;
+		while (start < end) {
+			System.out.println(-targetSum + ", " + arr[start] + ", " + arr[end]);
+			int currentSum = arr[start] + arr[end];
+			if (currentSum == targetSum && !triplets.contains(Arrays.asList(-targetSum, arr[start], arr[end]))) {
+				triplets.add(Arrays.asList(-targetSum, arr[start], arr[end]));
+				start++;
+				end--;
+			} else if (currentSum > targetSum) {
+				end--;
+			} else {
+				start++;
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		// System.out.println(searchTriplets(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
-		System.out.println(searchTriplets(new int[] { -5, 2, -1, -2, 3 }));
-
+		//System.out.println("Triplets found : " + searchTriplets(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
+		System.out.println("Triplets found : " + searchTriplets(new int[] { 2, -1, -1, -2, 3, 3 }));
 	}
-
 }
