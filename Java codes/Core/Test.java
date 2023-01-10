d55a0c0dabeb2016c8d2648be4ddcdc9d31944cf
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-01-10 14:55:07 +0530

    Commit

--- Java codes/Core/Test.java
+++ Java codes/Core/Test.java
@@ -1,13 +1,14 @@
 package Core;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Test {
 
 	public static void main(String[] args) {
-
-		int a = 10;
-		int b = -2;
-		System.out.println(Math.abs(b));
+		String version = "8.1.08";
+		System.out.println(Arrays.stream(version.split("\\.")).map(s -> String.format("%02d", Integer.parseInt(s))).collect(Collectors.joining(".")));				
+        System.out.println(Arrays.stream(version.split("\\.")).map(s -> (s.length() <2) ? String.format("%02d", Integer.parseInt(s)): s).collect(Collectors.joining(".")));
+		System.out.println(version);
 	}
 }
