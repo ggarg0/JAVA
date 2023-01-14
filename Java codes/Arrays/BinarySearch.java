1339ae3f597c5c6a5a60d1702e462ccd59e81bea
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-01-14 10:15:06 +0530

    Commit

--- Java codes/Arrays/BinarySearch.java
+++ Java codes/Arrays/BinarySearch.java
@@ -3,30 +3,26 @@ package Arrays;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-
 		int[] arr = { 1, 3, 4, 6, 7, 8, 9 };
-		int find = 9;
+		int find = 8;
 		int indexFound = -1;
 
 		int start = 0;
 		int end = arr.length;
-		int mid = (start + end) / 2;
-
-		for (; start < end;) {
-			if (arr[mid] == find) {
+		int mid = 0;
+		
+		while (start < end) {
+			mid = (start + end)/2;
+			if(arr[mid] == find) {
 				indexFound = mid;
 				break;
 			}
-
-			if (find < arr[mid])
+			
+			if(arr[mid] > find)
 				end = mid;
 			else
-				start = mid + 1;
-
-			mid = (start + end) / 2;
-		}
-
-		System.out.println("Found at index : " + indexFound);
+				start = mid + 1;			
+		}		
+		System.out.println("Index found : " + indexFound);
 	}
-
 }
