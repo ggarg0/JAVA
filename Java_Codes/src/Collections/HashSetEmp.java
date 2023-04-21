2dac71b5657889a02592c3cca41c9e649f0ed980
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-04-21 07:03:28 +0530

    Update HashSetEmp.java

--- Java_Codes/src/Collections/HashSetEmp.java
+++ Java_Codes/src/Collections/HashSetEmp.java
@@ -2,48 +2,48 @@ package Collections;
 
 import java.util.*;
 
-class HashSetEmp{
-	private int empNumber;
-	private String name;
+class HashSetEmp {
+    private int empNumber;
+    private String name;
 
-HashSetEmp(int empNumber,String name){
-	this.empNumber=empNumber;
-	this.name=name;
-}
+    HashSetEmp(int empNumber, String name) {
+        this.empNumber = empNumber;
+        this.name = name;
+    }
 
-void disp(){
-	System.out.println("emp no = "+empNumber + " and name = "+name);
-}
+    void disp() {
+        System.out.println("emp no = " + empNumber + " and name = " + name);
+    }
 
-String getName(){
-	return (this.empNumber + " and " + this.name);
-}
+    String getName() {
+        return (this.empNumber + " and " + this.name);
+    }
 
-void setEmp(){
-	empNumber=50;
-	name="Gaurav";
-}
+    void setEmp() {
+        empNumber = 50;
+        name = "Gaurav";
+    }
 
-public static void main(String arg[]){
-	Set<HashSetEmp> hashset=new HashSet<HashSetEmp>();
+    public static void main(String arg[]) {
+        Set<HashSetEmp> hashset = new HashSet<HashSetEmp>();
 
-	HashSetEmp emp1=new HashSetEmp(10,"Mary");
-	emp1.disp();
+        HashSetEmp emp1 = new HashSetEmp(10, "Mary");
+        emp1.disp();
 
-	HashSetEmp emp2=new HashSetEmp(20,"Jerry");
-	emp2.disp();
-	hashset.add(emp1);
-	hashset.add(emp2);
-	emp2=emp1;
+        HashSetEmp emp2 = new HashSetEmp(20, "Jerry");
+        emp2.disp();
+        hashset.add(emp1);
+        hashset.add(emp2);
+        emp2 = emp1;
 
-	emp2.disp();
-	hashset.add(emp2);
-	emp1.setEmp();
+        emp2.disp();
+        hashset.add(emp2);
+        emp1.setEmp();
 
-	Iterator<HashSetEmp> iterator = hashset.iterator();
-    while (iterator.hasNext()){
-		HashSetEmp o= (HashSetEmp) iterator.next();
-		 System.out.println("emp = "+o.getName());
+        Iterator<HashSetEmp> iterator = hashset.iterator();
+        while (iterator.hasNext()) {
+            HashSetEmp o = (HashSetEmp) iterator.next();
+            System.out.println("emp = " + o.getName());
         }
-	}
+    }
 }
