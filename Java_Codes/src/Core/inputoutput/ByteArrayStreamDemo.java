1be44646b365605f35978fdf6146d2b794b0f22c
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-04-27 09:03:05 +0530

    Update ByteArrayStreamDemo.java

---
+++ Java_Codes/src/Core/inputoutput/ByteArrayStreamDemo.java
@@ -0,0 +1,44 @@
+package Core.inputoutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ByteArrayStreamDemo {
+	public static void main(String[] args) {
+		byte[] array = { 1, 2, 3, 4 };
+		try {
+			ByteArrayInputStream input = new ByteArrayInputStream(array);
+			System.out.print("The bytes read from the input stream: ");
+			for (int i = 0; i < array.length; i++) {
+				int data = input.read();
+				System.out.print(" " + data);
+			}
+			input.close();
+			System.out.println();
+
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			byte[] buffer = { 97, 98, 99, 100 };
+
+			output.write(buffer);
+			System.out.print("The bytes read from the output stream: : ");
+			for (byte b : output.toByteArray()) {
+				System.out.print(" " + b);
+			}
+			output.close();
+
+			System.out.println();
+			String data = "This is a line of text inside the string.";
+			byte[] arrayOut = data.getBytes();
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			out.write(arrayOut);
+			String streamData = out.toString();
+
+			System.out.println("Output stream: " + streamData);
+
+			out.close();
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+	}
+}
