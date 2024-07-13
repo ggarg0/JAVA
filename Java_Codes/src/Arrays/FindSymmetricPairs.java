a4ae871a50b7a9fab09c8dfb1381f90e224faed7
Gaurav Garg <ggarg.bhilai@gmail.com>
2024-07-13 22:48:33 +0530

    commit

--- Java_Codes/src/Arrays/FindSymmetricPairs.java
+++ Java_Codes/src/Arrays/FindSymmetricPairs.java
@@ -1,35 +1,33 @@
 package Arrays;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 
 public class FindSymmetricPairs {
 
-	public static String findSymmetric(int[][] arr) {
-		String result = "";
+	public static List<List<Integer>> findSymmetric(int[][] arr) {
+		List<List<Integer>> result = new ArrayList<>();
 		Map<Integer, Integer> items = new HashMap<Integer, Integer>();
 
 		for (int i = 0; i < arr.length; i++) {
 			int first = arr[i][0];
 			int second = arr[i][1];
-
 			Integer value = items.get(second);
 			if (value != null && value == first) {
-				result += "{" + second + ", " + value + "}";
+				result.add(Arrays.asList(second, value));
+				result.add(Arrays.asList(first, second));
 			} else {
 				items.put(first, second);
 			}
-
 		}
-
 		return result;
 	}
 
 	public static void main(String args[]) {
-
 		int[][] arr = { { 1, 2 }, { 3, 4 }, { 5, 9 }, { 4, 3 }, { 9, 5 } };
-		String symmetric = findSymmetric(arr);
-		System.out.println(symmetric);
-
+		System.out.println(findSymmetric(arr));
 	}
 }
